b28d7431aeb90d0030ccb3ebc08fdbced900417b
xjk971020 <823840954@qq.com>
2020-02-23 12:23:56 +0800

    美化404页面

--- src/views/notfound.vue
+++ src/views/notfound.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="notfound">
-    notfound
+    <h1>抱歉，没有找到该页面</h1>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'notfound'
-  }
+export default {
+  name: "notfound"
+};
 </script>
 
 <style lang="scss" scoped>
-  
+.notfound {
+  height: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
